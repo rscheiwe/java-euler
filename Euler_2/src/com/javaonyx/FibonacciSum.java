4ebfcbc48d1f4ae185c086e5a983df5c867ceb0d
rscheiwe <rscheiwe@gmail.com>
2019-02-03 09:51:30 -0500

    Euler 2 -- Beginning Solution

---
+++ Euler_2/src/com/javaonyx/FibonacciSum.java
@@ -0,0 +1,13 @@
+package com.javaonyx;
+
+public class FibonacciSum {
+    int n; //number to count Fibonacci sequence to
+
+    public int getN() { return n; }
+    public void setN( int n ) { this.n = n; }
+
+    public FibonacciSum( int n ) { this.n = n; }
+
+
+
+}
