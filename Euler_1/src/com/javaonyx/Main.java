e737ecadd0664775f90090a4205601d0f90a4304
rscheiwe <rscheiwe@gmail.com>
2019-04-11 22:37:27 -0400

    refactor

--- Euler_1/src/com/javaonyx/Main.java
+++ Euler_1/src/com/javaonyx/Main.java
@@ -5,25 +5,34 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
+/* 
+ * Simple attempt at multi-file workflow
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
+//    	Create instance of ThreeFive
         ThreeFive numTest = new ThreeFive(2000);
+//      Call runThreeFive() on instance
         int a = numTest.runThreeFive();
+//      Convert to String for display
         String str1 = Integer.toString(a);
-        
+//      Call display and set to shell
 		Display display = new Display();
 		Shell shell = new Shell(display);
+//		Set ThreeFive return sum to text
 		Text threeFiveResult = new Text(shell, SWT.CENTER);
-	    threeFiveResult.setText(str1);
-	    threeFiveResult.setBounds(47, 20, 100, 20);
+		threeFiveResult.setText(str1);
+//		Style
+		threeFiveResult.setBounds(47, 20, 100, 20);
+//		Set result as the header, too
 		shell.setText(str1);
 		shell.open();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) display.sleep();
 		}
 		display.dispose();
-//        System.out.println(a);
     }
 }
